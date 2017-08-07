cca9903103065938fcbf51d625e60a5217d3ff27
antaema <emanoelbruno22@gmail.com>
2017-08-06 21:15:18 -0300

    Scripts de intalacao

---
+++ Downloads/scripts/Codigos/icon.sh
@@ -0,0 +1,9 @@
+sudo add-apt repository ppa:noobslab/icons
+sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:rebuntu16/other-stuff
+sudo apt-get update
+sudo apt-get install ultra-fat-icons
+sudo apt-get install ultra-fat-icons-orange
+sudo apt-get install ultra-fat-icons-green
+sudo apt-get install gnome-tweak-tool
+
